5263a31a5d26101f4cf58ef61ae9214ea0dd0b35
강예린 <jjykhs0505@naver.com>
2024-09-30 00:18:58 +0900

    initial commit

---
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)  // JUnit 4를 사용할 때 추가해야 함
+@SpringBootTest
+public class JpashopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
