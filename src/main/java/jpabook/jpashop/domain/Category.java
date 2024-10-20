16f685fa96c20b28f528492f7ff841de5ca74d45
강예린 <yerin7786@gmail.com>
2024-10-20 23:12:17 +0900

    section3. 엔티티 클래스 개발2

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+        joinColumns = @JoinColumn(name = "category_id"), // 중간 테이블에 있는 category_id
+            inverseJoinColumns = @JoinColumn(name = "item_id")) // 관계형 DB에는 컬렉션 관계를 양쪽에서 가질 수 있는 게 아니기 때문에 일대다, 다대일로 풀어내는 중간 테이블이 있어야 한다. (실무에서는 거의 못 씀)
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
