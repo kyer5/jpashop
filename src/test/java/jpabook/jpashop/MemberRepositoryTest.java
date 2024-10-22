bd16446cfa26727023bf85c58c161b84e1dc00ac
강예린 <yerin7786@gmail.com>
2024-10-22 18:38:32 +0900

    .

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 public class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     @Transactional // EntityManager를 통한 모든 데이터 변경은 항상 Transaction 안에서 이루어져야 한다.
@@ -25,11 +27,9 @@ public class MemberRepositoryTest {
         // given
         Member member = new Member();
         member.setUsername("memberA");
-
         // when
         Long saveId = memberRepository.save(member);
         Member findMember = memberRepository.find(saveId);
-
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
